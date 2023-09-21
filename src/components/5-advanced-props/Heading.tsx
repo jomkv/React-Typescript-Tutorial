dc9815e843196bb936619c69a9efd99a05c8a4b2
Jom Karlo Verzosa <jomkarlov@gmail.com>
2023-09-21 11:50:15 +0800

    add: clarifications

--- src/components/5-advanced-props/Heading.tsx
+++ src/components/5-advanced-props/Heading.tsx
@@ -2,6 +2,9 @@ type HeadingProps = {
   children: string
 }
 
+// Expected Props Example:
+// <Heading>Any text you want</Heading>
+
 function Heading(props: HeadingProps) {
   return (
     <div>{props.children}</div>
--- src/components/5-advanced-props/Oscar.tsx
+++ src/components/5-advanced-props/Oscar.tsx
@@ -2,6 +2,11 @@ type OscarProps = {
   children: React.ReactNode
 }
 
+// Expected Props Example:
+//<Oscar>
+//  <AnotherElement></AnotherElement>
+//</Oscar>
+
 function Oscar(props: OscarProps) {
   return (
     <div>{props.children}</div>
