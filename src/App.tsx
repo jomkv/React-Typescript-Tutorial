774c97243afe79e365fae2762c3d992c32e23c39
Jom Karlo Verzosa <jomkarlov@gmail.com>
2023-09-21 12:33:42 +0800

    finished session 9

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,13 @@
 
 
 // Components
-import PersonList from "./components/PersonList";
+import LoggedIn from "./components/state/LoggedIn";
 
 function App() {
-  const personsArray = [
-    {
-      name: "walter",
-      age: 14
-    },
-    {
-      name: "white",
-      age: 42
-    },
-    {
-      name: "junior",
-      age: 18
-    }
-  ]
+  
   return (
-    <div className='container'>
-      
-    <PersonList persons={personsArray} />
+    <div className='container'>  
+      <LoggedIn />
     </div>
   );
 }
